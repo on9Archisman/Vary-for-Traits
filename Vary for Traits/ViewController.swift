6d97692cf766399cfd27069cef502256ceb276a0
Archisman Banerjee <archisman.tech@gmail.com>
2019-02-28 12:52:40 +0530

    Initial Commit

---
+++ Vary for Traits/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Vary for Traits
+//
+//  Created by Archisman Banerjee on 28/02/19.
+//  Copyright © 2019 Archisman Banerjee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
